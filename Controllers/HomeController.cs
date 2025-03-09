b785ce38c0c83071cddecbf82d49201a2d963a19
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2025-03-09 20:47:25 +0100

    retry mechanism

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleRetryMechanism.Helpers;
+
+namespace SimpleRetryMechanism.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var executeWithRetry = await RetryHelper.ExecuteWithRetry(GetFileData,
+                    3, TimeSpan.FromSeconds(2));
+                return Ok(executeWithRetry);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        private async Task<string> GetFileData()
+        {
+            var readFile = await System.IO.File.ReadAllTextAsync("hello.txt");
+            return readFile + " test";
+        }
+    }
+}
---
+++ Helpers/RetryHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleRetryMechanism.Helpers
+{
+    public class RetryHelper
+    {
+        public static async Task<T?> ExecuteWithRetry<T>(Func<Task<T>> operation, int maxRetries, TimeSpan delay)
+        {
+            for (int i = 0; i < maxRetries; i++) 
+            {
+                try
+                {
+                    return await operation?.Invoke();
+                }
+                catch 
+                {
+                    await Task.Delay(delay);
+                }
+                if(i == maxRetries -1)
+                {
+                    throw new Exception("Failed all retries!");
+                }
+            }
+            return default(T?);
+        }
+    }
+}
